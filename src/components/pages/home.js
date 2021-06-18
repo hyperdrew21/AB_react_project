e9b1c4ff1a28107d6d25081cba1a5beb42f81a08
hyperdrew21 <angryfiredragon@gmail.com>
2021-06-17 22:37:04 -0400

    before draftjs

---
+++ src/components/pages/home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PortfolioContainer from "../portfolio/portfolio_container";
+
+export default function() {
+    return (
+        <div>
+            
+            <PortfolioContainer />
+        </div>
+        );
+}
